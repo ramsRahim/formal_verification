3357a32eb456a37b9ae65621099b57c1fb5d4a1c
ramsRahim <rahimhossain23@gmail.com>
2023-02-21 10:04:40 -0500

    vgg11 model tested with cifar10 dataset

--- main.py
+++ main.py
@@ -1,88 +1,87 @@
-# Load in relevant libraries, and alias where appropriate
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-from models.Lenet5 import LeNet5
+from models.Vgg11 import VGG11
+import random
+import numpy as np
 
-# Define relevant variables for the ML task
-batch_size = 64
-num_classes = 10
-learning_rate = 0.001
-num_epochs = 10
+random_seed = 43
+# Set random seed for reproducibility
+random.seed(random_seed)
+np.random.seed(random_seed)
+torch.manual_seed(random_seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
-# Device will determine whether to run the training on GPU or CPU.
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# Set device to GPU if available, otherwise use CPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-#Loading the dataset and preprocessing
-train_dataset = torchvision.datasets.MNIST(root = './data',
-                                           train = True,
-                                           transform = transforms.Compose([
-                                                  transforms.Resize((32,32)),
-                                                  transforms.ToTensor(),
-                                                  transforms.Normalize(mean = (0.1307,), std = (0.3081,))]),
-                                           download = True)
+# Define hyperparameters
+num_epochs = 50
+batch_size = 128
+learning_rate = 0.0001
 
+# Load and transform CIFAR-10 data
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
 
-test_dataset = torchvision.datasets.MNIST(root = './data',
-                                          train = False,
-                                          transform = transforms.Compose([
-                                                  transforms.Resize((32,32)),
-                                                  transforms.ToTensor(),
-                                                  transforms.Normalize(mean = (0.1325,), std = (0.3105,))]),
-                                          download=True)
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
 
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 
-train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-                                           batch_size = batch_size,
-                                           shuffle = True)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
 
+# Define VGG11 model and optimizer
+model = VGG11(num_classes=10).to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+# Define loss function
+criterion = nn.CrossEntropyLoss()
 
-test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
-                                           batch_size = batch_size,
-                                           shuffle = True)
-
-model = LeNet5(num_classes).to(device)
-
-#Setting the loss function
-cost = nn.CrossEntropyLoss()
-
-#Setting the optimizer with the model parameters and learning rate
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-#this is defined to print how many steps are remaining when training
-total_step = len(train_loader)
-
-## Training
+# Train the model
 total_step = len(train_loader)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):  
+    for i, (images, labels) in enumerate(train_loader):
         images = images.to(device)
         labels = labels.to(device)
-        
-        #Forward pass
+
+        # Forward pass
         outputs = model(images)
-        loss = cost(outputs, labels)
+        loss = criterion(outputs, labels)
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        		
-        if (i+1) % 400 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-# Test the model
-# In test phase, we don't need to compute gradients (for memory efficiency)
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        if (i+1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+        
 
-    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+    # Test the model after each epoch
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
+        accuracy = 100 * correct / total
+        print('Test Accuracy of the model on the test images: {} %'.format(accuracy))
